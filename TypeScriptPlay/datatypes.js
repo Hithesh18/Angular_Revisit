204a1ab54ce05a996dd739fb8853cd6631d8aaa6
Hithesh C H <hitheshch2000@gmail.com>
2024-10-06 14:14:30 +0200

    DataTypes

---
+++ TypeScriptPlay/datatypes.js
@@ -0,0 +1,7 @@
+"use strict";
+let lname = 'asd';
+//lname=10;
+let LastName;
+LastName = "Hithesh";
+let newname = LastName.toUpperCase();
+console.log(newname);
---
+++ TypeScriptPlay/datatypes.ts
@@ -0,0 +1,6 @@
+let lname='asd';
+//lname=10;
+let LastName:string;
+LastName="Hithesh";
+let newname=LastName.toUpperCase();
+console.log(newname);
